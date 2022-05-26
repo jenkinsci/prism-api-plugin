cd31350f95f45c3da227d80dcd5d7f64feed1348
Tim Jacomb <timjacomb1+github@gmail.com>
2022-05-26 09:09:01 +0100

    Use recommended theme from theme manager

--- src/main/java/io/jenkins/plugins/prism/PrismTheme.java
+++ src/main/java/io/jenkins/plugins/prism/PrismTheme.java
@@ -1,6 +1,8 @@
 package io.jenkins.plugins.prism;
 
 import hudson.util.ListBoxModel;
+import io.jenkins.plugins.thememanager.Theme;
+import io.jenkins.plugins.thememanager.ThemeManagerPageDecorator;
 
 /**
  * Defines the active theme to be used when rendering the source code with Prism.
@@ -8,29 +10,43 @@ import hudson.util.ListBoxModel;
  * @author Ullrich Hafner
  */
 public enum PrismTheme {
-    PRISM("Default", "prism.css"),
-    COY("Coy", "prism-coy.css"),
-    DARK("Dark", "prism-dark.css"),
-    FUNKY("Funky", "prism-funky.css"),
-    OKAIDIA("Okaidia", "prism-okaidia.css"),
-    SOLARIZED_LIGHT("Solarized Light", "prism-solarizedlight.css"),
-    TOMORROW_NIGHT("Tomorrow Night", "prism-tomorrow.css"),
-    TWILIGHT("Twilight", "prism-twilight.css");
+    PRISM("Recommended", null),
+    COY("Coy", "coy"),
+    DARK("Dark", "dark"),
+    FUNKY("Funky", "funky"),
+    OKAIDIA("Okaidia", "okaidia"),
+    SOLARIZED_LIGHT("Solarized Light", "solarizedlight"),
+    TOMORROW_NIGHT("Tomorrow Night", "tomorrow"),
+    TWILIGHT("Twilight", "twilight");
 
     private final String displayName;
-    private final String fileName;
+    private final String id;
 
-    PrismTheme(final String displayName, final String fileName) {
+    PrismTheme(final String displayName, final String id) {
         this.displayName = displayName;
-        this.fileName = fileName;
+        this.id = id;
     }
 
     public String getDisplayName() {
         return displayName;
     }
 
+    private String getId() {
+        if (this == PrismTheme.PRISM) {
+            Theme theme = ThemeManagerPageDecorator.get().findTheme();
+            return theme.getProperty("prism-api", "theme").orElse("prism");
+        }
+
+        return id;
+    }
+
     public String getFileName() {
-        return fileName;
+        String id = getId();
+        if (id.equals("prism")) {
+            return "prism.css";
+        }
+
+        return "prism-" + getId() + ".css";
     }
 
     /**
