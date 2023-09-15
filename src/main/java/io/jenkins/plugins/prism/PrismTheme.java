a87ebe07aa52a23a10ef5d6685e82f58ac1021ba
Ulli Hafner <ullrich.hafner@gmail.com>
2023-09-15 14:26:14 +0200

    Make it clear that default automatically is dark mode aware.

--- src/main/java/io/jenkins/plugins/prism/PrismTheme.java
+++ src/main/java/io/jenkins/plugins/prism/PrismTheme.java
@@ -8,7 +8,7 @@ import hudson.util.ListBoxModel;
  * @author Ullrich Hafner
  */
 public enum PrismTheme {
-    PRISM("Default", "prism.css"),
+    PRISM("Default (light and dark mode)", "prism.css"),
     COY("Coy", "prism-coy.css"),
     DARK("Dark", "prism-dark.css"),
     FUNKY("Funky", "prism-funky.css"),
