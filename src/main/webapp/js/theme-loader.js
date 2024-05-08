d183c397c2a59a052cab9b65ef123b17656d6c63
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-05-08 09:53:19 +0100

    Fix theme loading

--- src/main/webapp/js/theme-loader.js
+++ src/main/webapp/js/theme-loader.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     head.insertAdjacentHTML(
       'beforeend',
-      `<link id="prism-active-theme" rel="stylesheet" href="${baseUrl}/${themeCss}" />`)
+      `<link id="prism-active-theme" rel="stylesheet" href="${baseUrl}${themeCss}" />`)
   }
 
   if (window.getThemeManagerProperty && window.isSystemRespectingTheme) {
